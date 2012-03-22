86d9df14a42b0c7833b80c8bb5eaf2758358ae0a
José Luis Lafuente <joseluis@japeto.com>
2012-03-22 11:10:48 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+setup(
+    name = "CmakePy",
+    version = "0.1",
+    packages = find_packages(),
+    #scripts = ['cmake.py'],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['Jinja2>=2.6', 'distribute'],
+
+    
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        #'': ['*.txt', '*.cpp'],
+        'cmakepy.templates': ['*.txt', '*.cpp'],
+        # And include any *.msg files found in the 'hello' package, too:
+        #'templates': ['*.txt','*.cpp'],
+    },
+    
+    entry_points = {
+        'console_scripts': [
+            'cmake.py = cmakepy.cmakepy:main',
+        ]
+    },
+    
+    
+    #include_package_data = True,
+
+    # metadata for upload to PyPI
+    author = "Jose Luis Lafuente",
+    author_email = "joseluis@japeto.com",
+    description = "Creates a basic cmake project",
+    license = "GPLv3",
+    keywords = "cmake",
+    url = "http://example.com/",   # project home page, if any
+
+    #include_package_data = True
+    # could also include long_description, download_url, classifiers, etc.
+) 
